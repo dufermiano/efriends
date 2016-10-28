5c2719ae88c1e5c64deeb6ed5d17b3db5980af1e
dufermiano <dufermiano43@gmail.com>
2016-10-28 09:38:05 -0200

    Alterações no db session, precisa fazer

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://efriends.net.br/';
+$config['base_url'] = 'http://localhost/efriends';
 
 /*
 |--------------------------------------------------------------------------
@@ -367,10 +367,10 @@ $config['encryption_key'] = 'REALLY_LONG_NUMBER';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 1800;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
---
+++ zip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+zip -r efriends.zip application/ arquivos/ assets/ system/ composer.json composer.lock index.php .htaccess 
+
